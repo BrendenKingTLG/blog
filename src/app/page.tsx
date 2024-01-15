df390842d1dd5031081bd0ab3c51b79a625bfdca
BrendenKingTLG <kingbren@oregonstate.edu>
2024-01-15 13:55:47 -0600

    added blog endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,21 +163,21 @@ export default function Home() {
                     <textarea
                       name="message"
                       id="message"
-                      rows={4}
+                      rows={10}
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       defaultValue={""}
                     />
                   </div>
                 </div>
-                <div className="mt-10">
-                  <button
-                    type="submit"
-                    className="block w-full rounded-md bg-red-600 px-3.5 py-5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Submit
-                  </button>
-                </div>
               </div>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-red-600 px-3.5 py-5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Submit
+              </button>
             </div>
           </form>
         </div>
