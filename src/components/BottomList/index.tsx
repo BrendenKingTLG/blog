2da727e26f67aa84ba7c5253b7e3b317db37c85c
BrendenKingTLG <kingbren@oregonstate.edu>
2024-01-14 21:17:44 -0600

    added blog endpoint

--- src/components/BottomList/index.tsx
+++ src/components/BottomList/index.tsx
@@ -6,19 +6,6 @@ export default function index() {
       <div className="divider m-0"></div>
       <div className="flex flex-row gap-5 overflow-x-auto py-2">
         <div className="Greetings person, How Are You">
-          <div className="card w-60 shadow-lg border-2 border-gray-400 border-t-0">
-            <div className="card-body">
-              <h2 className="card-title text-xs">
-                Greetings person, How Are You
-              </h2>
-              <p className="text-xs">Hello its nice to meet you</p>
-              <div className="card-actions justify-end">
-                <p className="text-xs">07/2022 * Brenden King</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="Greetings person, How Are You">
           <div className="card w-60 shadow-lg border-2 border-gray-400">
             <div className="card-body">
               <h2 className="card-title text-xs">
@@ -84,7 +71,20 @@ export default function index() {
           </div>
         </div>
         <div className="Greetings person, How Are You">
-          <div className="card w-60 shadow-lg border-2 border-gray-400 border-2 border-gray-400">
+          <div className="card w-60 shadow-lg border-2 border-gray-400">
+            <div className="card-body">
+              <h2 className="card-title text-xs">
+                Greetings person, How Are You
+              </h2>
+              <p className="text-xs">Hello its nice to meet you</p>
+              <div className="card-actions justify-end">
+                <p className="text-xs">07/2022 * Brenden King</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Greetings person, How Are You">
+          <div className="card w-60 shadow-lg border-2 border-gray-400">
             <div className="card-body">
               <h2 className="card-title text-xs">
                 Greetings person, How Are You
