923cb3ac4fd2e3bed1099148dfe8eaa2879a2269
BrendenKingTLG <kingbren@oregonstate.edu>
2024-01-15 00:35:33 -0600

    added blog endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
                     href="#"
-                    className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-red-600 px-3.5 py-2.5 text-2xl font-semibold text-white"
                   >
                     Get started
                   </a>
