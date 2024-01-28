b35f6617b40c37c071a3801558827fec68dc3f86
BrendenKingTLG <kingbren@oregonstate.edu>
2024-01-28 12:50:11 -0600

    added mailer

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-
+import "./style.css";
 export default function Index() {
   const [email, setEmail] = useState("");
   const [sent, setSent] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,6 +20,12 @@ export default function Index() {
         if (response.ok) {
           // Email sent successfully, you can handle success here
           console.log("Email sent successfully");
+          setShowPopup(true); // Show the pop-up
+
+          // Set a timeout to hide the pop-up after 2 seconds
+          const timeoutId = setTimeout(() => {
+            setShowPopup(false);
+          }, 2000);
         } else {
           // Handle error when the email fails to send
           console.error("Error sending email");
@@ -34,6 +41,24 @@ export default function Index() {
 
   return (
     <div className="w-screen h-fit splash-screen">
+      {showPopup && (
+        <div className="popup flex flex-row justify-center items-center gap-1 rounded-md">
+          <p className="text-sm">sent</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="10"
+              height="10"
+              viewBox="0 0 50 50"
+            >
+              <path d="M 25 2 C 12.317 2 2 12.317 2 25 C 2 37.683 12.317 48 25 48 C 37.683 48 48 37.683 48 25 C 48 20.44 46.660281 16.189328 44.363281 12.611328 L 42.994141 14.228516 C 44.889141 17.382516 46 21.06 46 25 C 46 36.579 36.579 46 25 46 C 13.421 46 4 36.579 4 25 C 4 13.421 13.421 4 25 4 C 30.443 4 35.393906 6.0997656 39.128906 9.5097656 L 40.4375 7.9648438 C 36.3525 4.2598437 30.935 2 25 2 z M 43.236328 7.7539062 L 23.914062 30.554688 L 15.78125 22.96875 L 14.417969 24.431641 L 24.083984 33.447266 L 44.763672 9.046875 L 43.236328 7.7539062 z"></path>
+            </svg>
+          </div>
+        </div>
+      )}
+
       <div className="relative isolate px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-20">
           <div className="hero min-h-fit bg-base-100 rounded-md p-6 min-w-3.5">
@@ -75,14 +100,9 @@ export default function Index() {
                           y="0px"
                           width="20"
                           height="20"
-                          viewBox="0 0 24 24"
+                          viewBox="0 0 50 50"
                         >
-                          <path d="M19.8,10.4c0.1,0.5,0.2,1.1,0.2,1.6c0,4.4-3.6,8-8,8s-8-3.6-8-8s3.6-8,8-8c1.6,0,3.2,0.5,4.4,1.3l1.4-1.4 C16.2,2.7,14.2,2,12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10c0-1.1-0.2-2.2-0.5-3.2L19.8,10.4z"></path>
-                          <path d="M11 16.4L6.3 11.7 7.7 10.3 11 13.6 21.3 3.3 22.7 4.7z"></path>
-                          <path
-                            d="M12 2A10 10 0 1 0 12 22A10 10 0 1 0 12 2Z"
-                            opacity=".3"
-                          ></path>
+                          <path d="M 25 2 C 12.317 2 2 12.317 2 25 C 2 37.683 12.317 48 25 48 C 37.683 48 48 37.683 48 25 C 48 20.44 46.660281 16.189328 44.363281 12.611328 L 42.994141 14.228516 C 44.889141 17.382516 46 21.06 46 25 C 46 36.579 36.579 46 25 46 C 13.421 46 4 36.579 4 25 C 4 13.421 13.421 4 25 4 C 30.443 4 35.393906 6.0997656 39.128906 9.5097656 L 40.4375 7.9648438 C 36.3525 4.2598437 30.935 2 25 2 z M 43.236328 7.7539062 L 23.914062 30.554688 L 15.78125 22.96875 L 14.417969 24.431641 L 24.083984 33.447266 L 44.763672 9.046875 L 43.236328 7.7539062 z"></path>
                         </svg>
                       )}
                     </button>
