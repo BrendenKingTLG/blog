1b5f6a2e1a68533f577fd8820f1b5829d2c39b42
BrendenKingTLG <kingbren@oregonstate.edu>
2024-01-28 10:09:38 -0600

    added mailer

--- src/components/BlogContainer/index.tsx
+++ src/components/BlogContainer/index.tsx
@@ -11,13 +11,15 @@ type BlogPost = {
 
 export default function BlogPost({ post }: { post: BlogPost }) {
   return (
-    <div className="blog-post">
+    <div>
       <div className="divider"></div>
-      <h1>{post.title}</h1>
-      <p className="author">Author: {post.author}</p>
-      <p className="date">Date: {post.post_date.slice(0, 10)}</p>
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      <div className="blog-post">
+        <h1>{post.title}</h1>
+        <p className="author">Author: {post.author}</p>
+        <p className="date">Date: {post.post_date.slice(0, 10)}</p>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
     </div>
   );
 }
